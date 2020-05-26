b8ab0ee96b11a8aa595b2023d6db093d2752a907
lichaofeng <lichaofeng@qq.com>
2020-05-26 15:35:24 +0800

    ExampleDbContext

--- Example/Example.EntityFramwork/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Example.EntityFramwork
-{
-    public class Class1
-    {
-    }
-}
---
+++ Example/Example.EntityFramwork/ExampleDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Example.Model;
+using Example.Model.Table.Sys;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Example.EntityFramwork
+{
+    public class ExampleDbContext : DbContext
+    {
+        public ExampleDbContext(DbContextOptions<ExampleDbContext> options)
+           : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var item in modelBuilder.Model.GetEntityTypes().Where(e => typeof(BaseEntity).IsAssignableFrom(e.ClrType)))
+            {
+                if (modelBuilder.Entity(item.ClrType).Property("IsDelete") == null)
+                {
+                    continue;
+                }
+                modelBuilder.Entity(item.ClrType).Property<Boolean>("IsDelete");
+                var parameter = Expression.Parameter(item.ClrType, "e");
+                var body = Expression.Equal(Expression.Call(typeof(EF), nameof(EF.Property), new[] { typeof(bool) }, parameter, Expression.Constant("IsDelete")), Expression.Constant(false));
+                modelBuilder.Entity(item.ClrType).HasQueryFilter(Expression.Lambda(body, parameter));
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #region DbSet
+
+        public DbSet<User> Sys_User { get; set; }
+        #endregion
+    }
+}
