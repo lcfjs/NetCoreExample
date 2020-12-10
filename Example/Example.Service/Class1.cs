adc28e5167612195b15740b49118099028ff8b59
lichaofeng <viplichaofeng@163.com>
2020-12-10 15:14:54 +0800

    Update and rename Class1.cs to Services.cs
    httpContextAccessor.HttpContext.RequestServices

--- Example/Example.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Example.Service
-{
-    public class Class1
-    {
-    }
-}
---
+++ Example/Example.Service/Services.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Example.Service
+{
+    public interface IServices
+    {
+        T CreateService<T>() where T : class;
+
+        /// <summary>
+        /// AddScoped时单独开辟新生命周期
+        /// </summary>
+        /// <returns></returns>
+        IServiceScope CreateScope();
+    }
+    public class Services : IServices
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public Services(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public T CreateService<T>() where T : class
+        {
+            //httpContextAccessor.HttpContext.RequestServices.GetRequiredService
+            return httpContextAccessor.HttpContext.RequestServices.GetService<T>();
+        }
+
+        public IServiceScope CreateScope() 
+        {
+            return httpContextAccessor.HttpContext.RequestServices.CreateScope();
+        }
+    }
+}
